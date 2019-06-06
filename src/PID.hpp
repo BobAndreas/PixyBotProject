740e7a6a967fa858526c6ad1f3ccd316f44f2dc0
pstddy <p.s.tadday@online.de>
2019-06-06 16:24:18 +0200

    Added cpp file

--- src/PID.hpp
+++ src/PID.hpp
@@ -4,7 +4,7 @@ struct PID_Config {
   int p, i, d, target, divider, lowerBound, upperBound;   
         
 };
-    
+
 class PID{
   
   public:
@@ -24,50 +24,3 @@ class PID{
     int historyScale;
 
 };
-
-PID::PID(PID_Config config){
-  
-  this->config = config;
-  historyScale = 1;
-  for(int i = 2; i <= BUFFER_SIZE; i++)
-    historyScale *= i;
-  clearBuf();
-}
-
-int PID::next(int measured){
-  int difference = measured - config.target;
-  int sum = 0;
-  int scale = 1;
-  for(int i = index; i < BUFFER_SIZE; i++){
-    sum += valueBuf[(i + index)%BUFFER_SIZE] * (BUFFER_SIZE - i) ;
-  }
-  
-  currentValue = (config.p * difference + config.i * sum / historyScale + config.d * (difference - valueBuf[index])) / config.divider;
-
-  if (currentValue < config.lowerBound) currentValue = config.lowerBound;
-  if (currentValue > config.upperBound) currentValue = config.upperBound;
- 
-  
-  index = (index + 1) % BUFFER_SIZE;
-  
-  valueBuf[index] = difference;
-  
-  return currentValue;
-}
-
-int PID::current(){
-  return currentValue;
-}
-
-void PID::setTarget(int target){
-  config.target = target;
-}
-
-void PID::clearBuf(){
-
-  index = 0;
-  currentValue = 0;
-  for(int i = 0; i < BUFFER_SIZE; i++){
-    valueBuf[i] = 0;
-  } 
-}
